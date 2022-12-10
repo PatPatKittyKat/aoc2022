ff1872a657e3de735f1bd4ff02eaf64e4260fe4c
Anthony Cooper <1anthonycooper1@gmail.com>
2022-12-09 18:30:14 -0600

    commit setup

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace aoc2022
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Attempting Advent of Code for fun :)");
+            Console.WriteLine("testing commit");
+        }
+    }
+}
