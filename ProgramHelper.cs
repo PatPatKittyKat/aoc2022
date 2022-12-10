66883539c7d4abf9b17ff59ecde2201efa9486d9
Anthony Cooper <1anthonycooper1@gmail.com>
2022-12-10 10:05:21 -0600

    commit - base set-up and solved day 1

---
+++ ProgramHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace aoc2022
+{
+    public class ProgramHelper
+    {
+        public static string BuildProblemMessage(int problem)
+        {
+            string problemName;
+            switch (problem)
+            {
+                case 1:
+                    problemName = ProblemStringFormatHelper(problem, "Calorie Counting");
+                    break;
+                case 2:
+                    problemName = ProblemStringFormatHelper(problem, "Rock Paper Scissors");
+                    break;
+                default:
+                    problemName = "Could not find problem name";
+                    break;
+            };
+
+            return problemName;
+        }
+
+        private static string ProblemStringFormatHelper(int day, string name)
+        {
+            return string.Concat("Day ", day, ": ", name);
+        }
+    }
+}
